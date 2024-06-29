4caab9785b15b01995059bc8bf7a80466e728e32
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-06-29 14:54:19 -0300

    feat: initial refactor code

--- WorldCupSimulator/Interface/IPhase.cs
+++ WorldCupSimulator/Interface/IPhase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorldCupSimulator.Interface
+﻿namespace WorldCupSimulator.Interface
 {
     public interface IPhase
     {
