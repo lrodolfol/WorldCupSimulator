360dd417a02e215925cb1e735fd485cdaffab182
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-07-15 18:59:45 -0300

    initial persist matches

---
+++ WorldCupSimulator/Util/DataLogs.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldCupSimulator.Util;
+public record class DataLogs
+{
+    public string Description { get; set; } = null!;
+    public Team TeamOne { get; set; } = null!;
+    public Team TeamTwo { get; set; } = null!;
+    public Team Winner { get; set; } = null!;
+}
---
+++ WorldCupSimulator/Util/EventsLogs.cs
@@ -0,0 +1,45 @@
+﻿using ClosedXML.Excel;
+
+namespace WorldCupSimulator.Util;
+public static class EventsLogs
+{
+    public static List<DataLogs> Data { get; set; } = new();
+
+    public static void Generate()
+    {
+        using(var workbook = new XLWorkbook())
+        {
+            string filePathName = System.IO.Directory.GetCurrentDirectory() + "\\" + "EventsLogs" + DateTime.Now;
+            if (File.Exists(filePathName))
+                File.Delete(filePathName);
+
+            var planilha = workbook.Worksheets.Add($"Partida");
+
+            int line = 1;
+            GenerateHeader(planilha);
+
+            line++;
+
+            foreach (var item in Data)
+            {
+                planilha.Cell("A" + line).Value = item.TeamOne.Name;
+                planilha.Cell("B" + line).Value = item.TeamTwo.Name;
+                planilha.Cell("C" + line).Value = item.Winner.Name;
+                planilha.Cell("D" + line).Value = item.Description;
+                line++;
+            }
+            workbook.SaveAs(filePathName);
+        }
+    }
+
+    private static void GenerateHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = "Código";
+        worksheet.Cell("B1").Value = "Fornecedor";
+        worksheet.Cell("C1").Value = "Valor R$";
+        worksheet.Cell("D1").Value = "Vencimento";
+        worksheet.Cell("E1").Value = "Pagamento";
+        worksheet.Cell("F1").Value = "Valor Pago";
+        worksheet.Cell("G1").Value = "Descrição";
+    }
+}
